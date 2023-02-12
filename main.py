7f52376b361fdbb156638d56c93dcc52927bbff7
YourUsername <phusanakan.p@ku.th>
2023-02-12 21:44:13 +0700

    backend-fast-api-assignment-1

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ MONGO_DB_PORT = 27017
 
 
 class Reservation(BaseModel):
-    name : str
+    name: str
     start_date: date
     end_date: date
     room_id: int
@@ -26,13 +26,14 @@ app = FastAPI()
 
 
 def room_avaliable(room_id: int, start_date: str, end_date: str):
-    query={"room_id": room_id,
-           "$or": 
-                [{"$and": [{"start_date": {"$lte": start_date}}, {"end_date": {"$gte": start_date}}]},
-                 {"$and": [{"start_date": {"$lte": end_date}}, {"end_date": {"$gte": end_date}}]},
-                 {"$and": [{"start_date": {"$gte": start_date}}, {"end_date": {"$lte": end_date}}]}]
-            }
-    
+    query = {"room_id": room_id,
+             "$or":
+             [{"$and": [{"start_date": {"$lte": start_date}}, {"end_date": {"$gte": start_date}}]},
+              {"$and": [{"start_date": {"$lte": end_date}},
+                        {"end_date": {"$gte": end_date}}]},
+              {"$and": [{"start_date": {"$gte": start_date}}, {"end_date": {"$lte": end_date}}]}]
+             }
+
     result = collection.find(query, {"_id": 0})
     list_cursor = list(result)
 
@@ -40,21 +41,75 @@ def room_avaliable(room_id: int, start_date: str, end_date: str):
 
 
 @app.get("/reservation/by-name/{name}")
-def get_reservation_by_name(name:str):
-    pass
+def get_reservation_by_name(name: str):
+    query = {"name": name}
+
+    result = collection.find(query, {'_id': 0})
+    list_cursor = list(result)
+
+    return {
+        "result": list_cursor
+    }
+
 
 @app.get("/reservation/by-room/{room_id}")
 def get_reservation_by_room(room_id: int):
-    pass
+    query = {"room_id": room_id}
+
+    result = collection.find(query, {'_id': 0})
+    list_cursor = list(result)
+
+    return {
+        "result": list_cursor
+    }
+
 
 @app.post("/reservation")
-def reserve(reservation : Reservation):
-    pass
+def reserve(reservation: Reservation):
+    if reservation.end_date < reservation.start_date or reservation.room_id < 0 or reservation.room_id > 10:
+        raise HTTPException(status_code=400)
+
+    if not room_avaliable(reservation.room_id, reservation.start_date.strftime("%Y-%m-%d"), reservation.end_date.strftime("%Y-%m-%d")):
+        raise HTTPException(status_code=400)
+    collection.insert_one({
+        "name": reservation.name,
+        "start_date": reservation.start_date.strftime("%Y-%m-%d"),
+        "end_date": reservation.end_date.strftime("%Y-%m-%d"),
+        "room_id": reservation.room_id,
+    })
+    return
+
 
 @app.put("/reservation/update")
 def update_reservation(reservation: Reservation, new_start_date: date = Body(), new_end_date: date = Body()):
+    if new_start_date > new_end_date:
+        raise HTTPException(status_code=400)
+
+    if not room_avaliable(reservation.room_id, new_start_date.strftime("%Y-%m-%d"), new_end_date.strftime("%Y-%m-%d")):
+        raise HTTPException(status_code=400)
+    
+    query = {
+        "name": reservation.name,
+        "start_date": reservation.start_date.strftime("%Y-%m-%d"),
+        "end_date": reservation.end_date.strftime("%Y-%m-%d"),
+        "room_id": reservation.room_id,
+    }
+    collection.update_one(query, {
+        "$set": {
+            "start_date": new_start_date.strftime("%Y-%m-%d"),
+            "end_date": new_end_date.strftime("%Y-%m-%d")
+        }
+    })
     pass
 
-@app.delete("/reservation/delete")
+
+@ app.delete("/reservation/delete")
 def cancel_reservation(reservation: Reservation):
+    query = {
+        "name": reservation.name,
+        "start_date": reservation.start_date.strftime("%Y-%m-%d"),
+        "end_date": reservation.end_date.strftime("%Y-%m-%d"),
+        "room_id": reservation.room_id,
+    }
+    collection.delete_one(query)
     pass
